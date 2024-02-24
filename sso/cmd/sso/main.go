1da7662281b6a8428f1926c685cd350b301dc35a
jellyfisharuka <arukeulen@gmail.com>
2024-02-25 02:19:43 +0600

    logging. slog package

--- sso/cmd/sso/main.go
+++ sso/cmd/sso/main.go
@@ -1,15 +1,46 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 	"sso/internal/config"
 )
-
+const (
+	envLocal = "local"
+	envDev="dev"
+	envProd="prod"
+)
 func main() {
 	//TODO: инициализировать конфиг
 	cfg := config.MustLoad()
-	fmt.Print(cfg)
+	log:=setupLogger(cfg.Env)
+	log.Info("starting application",
+	 slog.String("env", cfg.Env),
+	 slog.Any("cfg", cfg),
+	 slog.Int("port", cfg.Grpc.Port),
+	)
+	log.Debug("debug message")
+	log.Error("error message")
+	log.Warn("warn message")
 	//TODO: инициализировать логгер
 	//TODO: инициализировать приложение app
 	//TODO: запустить gRPC-сервер приложения
+}
+func setupLogger(env string) *slog.Logger {
+var log *slog.Logger
+switch env {
+case envLocal:
+	log=slog.New(
+		slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level:slog.LevelDebug}),
+	)
+case envDev:
+	log=slog.New(
+		slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+	)
+case envProd:
+	log=slog.New(
+		slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level:slog.LevelInfo}),
+	)
+}
+return log
 }
