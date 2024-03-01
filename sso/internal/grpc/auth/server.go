1d5d4e685928f657cb578bcc4e2730bc844a1b78
jellyfisharuka <arukeulen@gmail.com>
2024-03-01 23:58:51 +0500

    handler login

--- sso/internal/grpc/auth/server.go
+++ sso/internal/grpc/auth/server.go
@@ -8,14 +8,18 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
-type Server interface {
+type Auth interface {
+	Login(ctx context.Context, email string, password string, appID int ) (token string, err error) 
+	Register(ctx context.Context, email string, password string)(userID int, err error) 
+	isAdmin(ctx context.Context, userID int)(b bool, err error)
 	
 }
 type ServerAPI struct {
 	ssov1.UnimplementedAuthServer
+	auth Auth
 }
 
-func Register(gRPC *grpc.Server) {
+func Register(gRPC *grpc.Server, auth Auth) {
 	ssov1.RegisterAuthServer(gRPC, &ServerAPI{}) //регистрирует обработчик
 }
 const (
@@ -33,8 +37,13 @@ func (s *ServerAPI) Login(ctx context.Context, req *ssov1.LoginRequest,
        return nil, status.Error(codes.InvalidArgument, "app_id is required")
 	}
   //TODO: implement login via auth service
+   token, err:= s.auth.Login(ctx, req.GetEmail(), req.GetPassword(), int(req.GetAppId())); 
+	if err!=nil {
+		return nil, status.Error(codes.Internal, "internal error")
+  }
+  
 	return &ssov1.LoginResponse{
-		Token: req.GetEmail(), //можно и просто req.Email() разницы нет, но чисто для интереса вернуть геттеры
+		Token: token, //можно и просто req.Email() разницы нет, но чисто для интереса вернуть геттеры
 	}, nil
 }
 func (s *ServerAPI) Register(ctx context.Context, req *ssov1.RegisterRequest,
